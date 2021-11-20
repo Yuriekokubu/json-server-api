4be9e38da07e3db79d87c78cd2aef1c12e24bc32
Yuriekokubu <60240233118@udru.ac.th>
2021-11-20 12:56:36 +0700

    json-server

---
+++ server.js
@@ -0,0 +1,40 @@
+var faker = require('faker');
+var jsonServer = require('json-server');
+
+function generatePerson(id) {
+  return {
+    id: id,
+    name: {
+      given: faker.name.firstName(),
+      family: faker.name.lastName(),
+    },
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    avatar: faker.internet.avatar(),
+    company: faker.company.companyName(),
+    address: {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zip: faker.address.zipCode(),
+      country: faker.address.country(),
+    },
+    phone: faker.phone.phoneNumber(),
+    note: faker.lorem.paragraph(),
+  };
+}
+
+function generateFakeData() {
+  var people = [];
+  for (var i = 1; i <= 20; i++) {
+    people.push(generatePerson(i));
+  }
+  return { people };
+}
+
+var people = generateFakeData();
+var server = jsonServer.create();
+server.use(jsonServer.defaults());
+var router = jsonServer.router(people);
+server.use(router);
+server.listen(5000);
